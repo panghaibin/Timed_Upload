82f6c421ad6a4bbdc43e9856841ba9c1c23cbaf9
panghaibin <i@hbte.ch>
2022-05-20 17:27:24 +0800

    init test

--- init.py
+++ init.py
@@ -6,6 +6,10 @@ import sqlite3
 with sqlite3.connect('database.db') as db:
     with open('schema.sql', 'r') as f:
         db.cursor().executescript(f.read())
+    # 测试
+    db.cursor().execute('INSERT INTO users (username, password, name, role_id) VALUES (?, ?, ?, ?)',
+                        ('admin', 'admin', '管理员', 1))
+    db.commit()
 
 key = os.urandom(24)
 random_img_folder = 'uploads/ag_img'
