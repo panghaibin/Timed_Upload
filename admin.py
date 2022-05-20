ff99204ecb2f29500e348e25b161e993cac10401
panghaibin <i@hbte.ch>
2022-05-20 15:18:32 +0800

    migrate img path

--- admin.py
+++ admin.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import yaml
+import sqlite3
 from utils import get_config, abs_path, config_path
 
 app_config = get_config(os.path.join(abs_path, config_path))
+DATABASE = os.path.join(abs_path, app_config['DATABASE'])
 
 
 def generate_secret_key():
@@ -20,6 +22,16 @@ def save_secret_key(key):
     app_config['SECRET_KEY'] = key
     with open(os.path.join(abs_path, config_path), 'w') as f:
         yaml.dump(app_config, f)
+
+
+def migrate_img_path():
+    pre_path = os.path.join(abs_path, '')
+    with sqlite3.connect(DATABASE) as db:
+        query = "UPDATE history SET test_img_path = REPLACE(test_img_path, ?, '') WHERE test_img_path like ?;"
+        db.cursor().execute(query, (pre_path, f'{pre_path}%'))
+        query = "UPDATE history SET test_cps_path = REPLACE(test_cps_path, ?, '') WHERE test_cps_path like ?;"
+        db.cursor().execute(query, (pre_path, f'{pre_path}%'))
+        db.commit()
 
 
 def main():
